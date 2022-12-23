f953bc7a138f5cc439c285e44fd89ff89d77f0f6
bilgeadamankaraboostjava3 <107560948+bilgeadamankaraboostjava3@users.noreply.github.com>
2022-12-23 10:50:10 +0300

    23.12.2022 2. ders

---
+++ src/main/java/org/randomuser/Runner.java
@@ -0,0 +1,51 @@
+package org.randomuser;
+
+import com.google.gson.*;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Runner {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("***** KULLANICI DATALARI ***********");
+        System.out.println("işlem başladı....");
+        Scanner sc = null;
+        sc = new Scanner(new URL("https://randomuser.me/api/?results=500").openStream(),"UTF-8");
+        StringBuilder sb = new StringBuilder();
+        String row;
+        while (sc.hasNext()){
+            row = sc.nextLine();
+            sb.append(row);
+        }
+        System.out.println("Bütün datalar çekildi.");
+
+        List<Person> kullaniciListesi = new ArrayList<>();
+        Gson gson = new Gson();
+        /**
+         * elimizde bulunan string halindeki json datayı jsonelement olarak parse ediyoruz.
+         */
+        JsonElement jsonElement = JsonParser.parseString(sb.toString());
+        /**
+         * Json elemen i bir jsonDataya çevirmeliyiz ki içinden bilgileri alalım
+         */
+        JsonObject jsonObject = jsonElement.getAsJsonObject();
+        /**
+         * Json onject e çevrilen datanını içniden artık istediğimiz key ile gerekli
+         * bilgileri çekebiliriz.
+         */
+        JsonArray jsonArray = jsonObject.get("results").getAsJsonArray();
+        int size = jsonArray.size();
+        for (int i=0;i<size;i++){
+            JsonObject p = jsonArray.get(i).getAsJsonObject();
+            Person person = gson.fromJson(p, Person.class);
+            kullaniciListesi.add(person);
+        }
+
+        kullaniciListesi.forEach(System.out::println);
+
+
+    }
+}
